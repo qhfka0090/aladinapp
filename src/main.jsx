7c81ae11863b33e0e6a2e8f62242feb15bff7a9e
qhfka0090 <qhfka0090@naver.com>
2025-08-12 17:49:59 +0900

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import store from './store';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App /> 
     </BrowserRouter>
   </Provider>
